d65e226c9b1e1fa1a67ba784b33be07cc921e990
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-09-08 23:31:30 -0500

    Creando una Safe Area para una interfaz que tiene un AppBar

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
+import 'package:platzi_trips_app/widgets/text_input.dart';
 import 'package:platzi_trips_app/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -21,11 +22,15 @@ class _AddPlaceScreen extends State<AddPlaceScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final _controllerTitlePlace = TextEditingController();
+    final _controllerDescriptionPlace = TextEditingController();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
           GradientBack(height: 300.0,),
-          Row(
+          Row( //APP BAR
             children: <Widget>[
               Container(
                 padding: EdgeInsets.only(top: 25.0, left: 5.0),
@@ -47,6 +52,29 @@ class _AddPlaceScreen extends State<AddPlaceScreen> {
                   )
               )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: <Widget>[
+                Container(), //FOTO
+                Container( //TextField Title
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: "Title",
+                    inputType: null,
+                    maxLines: 1,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                TextInput( //DESCRIPCION
+                    hintText: "Description",
+                    inputType: TextInputType.multiline,
+                    maxLines: 4,
+                    controller: _controllerDescriptionPlace
+                )
+              ],
+            ),
           )
         ],
       ),
